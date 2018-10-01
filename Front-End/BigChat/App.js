0b0a0b5670d6873015a0928f39da1188c19434cf
Harminder Kandola <harminder.kandola@sap.com>
2018-09-30 17:50:27 -0700

    Updating

--- Front-End/BigChat/App.js
+++ Front-End/BigChat/App.js
@@ -6,18 +6,83 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {AppRegistry,Platform, StyleSheet, Text, View} from 'react-native';
+// import React, {Component} from 'react';
+// import {AppRegistry,Platform, StyleSheet, Text, View} from 'react-native';
+// import Login from './src/components/Login/Login.js';
+// import ScrollList from './src/components/ScrollListExample/ScrollListExample.js'
+
+// export default class DemoLogin extends Component {
+//   render() {
+//     return (
+//       <ScrollList/>
+//     );
+//   }
+// }
+
+// AppRegistry.registerComponent('DemoLogin', () => DemoLogin);
+
+
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import Login from './src/components/Login/Login.js';
 import ScrollList from './src/components/ScrollListExample/ScrollListExample.js'
 
-
-export default class DemoLogin extends Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <ScrollList/>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Messages"
+          onPress={() => this.props.navigation.navigate('Messages')}
+        />
+        <Button
+          title="Go to Login"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('DemoLogin', () => DemoLogin);
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        header: null // Will hide header for Home only
+    }
+    },
+    Details: DetailsScreen,
+    Messages: ScrollList,
+    Login: Login,
+  },
+  {
+    // Hides Header globally
+    navigationOptions: {
+      header: null,
+    }
+   },
+  {
+    initialRouteName: 'Home',
+  },
+
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
