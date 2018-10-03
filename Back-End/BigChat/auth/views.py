51d1475b87a36c83db931e47f151fdcf592b20eb
Harminder Kandola <harminder.kandola@sap.com>
2018-10-03 16:13:20 -0700

    EOD commit

--- Back-End/BigChat/auth/views.py
+++ Back-End/BigChat/auth/views.py
@@ -1,9 +1,12 @@
 import firebase_admin
-from firebase_admin import credentials
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-import os 
 
+from firebase_admin import credentials
+from firebase_admin import auth
+from firebase_admin import firestore
+from 
 # Create your views here.
 
 cred = credentials.Certificate("./auth/serviceAccountKey.json")
@@ -21,6 +24,7 @@ class Authenticate( View ):
         return HttpResponse("Invalid Authenticate Request.")
 
     def post(self, request, auth_type=google):
+        firebase_admin.auth._get_auth_service
         return HttpResponse("Google Auth")
 
     def post(self, request, auth_type=fb):
